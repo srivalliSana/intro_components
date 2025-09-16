67e59a1214b837cb55585fc6f2ef809cdf2e27b0
SANA SRIVALLI <143521609+srivalliSana@users.noreply.github.com>
2025-09-16 21:11:58 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/content.jsx"
+
+
+export default function App(){
+  return(
+    <div>
+      <Header/>
+      <Content/>
+      <Footer/>
+
+    </div>
+  )
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div>
+            <h3 style={{alignContent:'right'}}>home  content</h3>
+        </div>
+    )
+};
